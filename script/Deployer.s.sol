1216eb86f8effab80c82ab343dcc6a94b594fd3b
0xkowalski <0xkowalskiaudit@gmail.com>
2024-03-09 17:48:00 +0000

    init

---
+++ script/Deployer.s.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "forge-std/Script.sol";
+import "forge-std/console.sol";
+import "../src/00_HelloEthernaut.sol";
+
+contract Deployer is Script {
+    function run() external {
+        vm.startBroadcast();
+
+        // 00
+        HelloEthernaut helloEthernaut = new HelloEthernaut();
+        console.log("Deployed 00_HelloEthernaut at address:", address(HelloEthernaut));
+
+        //01
+
+        vm.stopBroadcast();
+    }
+}
+
