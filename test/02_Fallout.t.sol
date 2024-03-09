2c8bd80645171aa8ffab1624e51c8faae1182530
0xKowalski <0xKowalskiAudit@gmail.com>
2024-03-09 22:20:08 +0000

    Fallout previously was made with 0.6.0, now uses 0.8.0

--- test/02_Fallout.t.sol
+++ test/02_Fallout.t.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.6.0;
+pragma solidity ^0.8.0;
 
 import "forge-std/Test.sol";
 import "../src/02_Fallout.sol";
@@ -21,6 +21,6 @@ contract REPLACETest is Test {
         vm.prank(attacker);
 
 
-        assertEq(_fallout.owner, attacker, "Atacker is not owner!");
+        assertEq(_fallout.owner(), attacker, "Atacker is not owner!");
     }
 }
