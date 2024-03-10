7ca5f1f822dba366a8e8e65e4f9d1d5722020cbb
0xkowalski <0xkowalskiaudit@gmail.com>
2024-03-10 14:06:56 +0000

    Unsolved 14 gatekeeper two added

---
+++ test/14_GatekeeperTwo.t.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "forge-std/Test.sol";
+import "../src/14_GatekeeperTwo.sol";
+
+contract GatekeeperTwoTest is Test {
+    GatekeeperTwo _gatekeeperTwo;
+    address deployer;
+    address attacker;
+
+    function setUp() public {
+        deployer = address(this);
+        attacker = vm.addr(1);
+
+        _gatekeeperTwo = new GatekeeperTwo();
+
+    }
+
+    function testSolution() public {
+        vm.startPrank(attacker);
+
+
+        assertTrue(_gatekeeperTwo.cleared(), "Level is not cleared!");
+    }
+}
